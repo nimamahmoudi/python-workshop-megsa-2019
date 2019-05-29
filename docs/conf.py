2c873d5c1930ec202c3fb4eadc8c3b69f962f669
Nima Mahmoudi <nima_mahmoudi@live.com>
2019-05-29 12:52:39 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ project = 'Python Workshop'
 copyright = '2019, Nima Mahmoudi'
 author = 'Nima Mahmoudi'
 master_doc = 'index'
-html_theme = "sphinx_rtd_theme"
 
 
 # -- General configuration ---------------------------------------------------
@@ -46,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
